858e38500d1c2497a75845ca87d72e4bdf394581
danielMonas <33154826+danielMonas@users.noreply.github.com>
2017-12-07 21:11:43 +0200

    Update Position.cpp

--- Position.cpp
+++ Position.cpp
@@ -42,10 +42,3 @@ int Position::getNumber() const
 {
 	return _number;
 }
-/**
-A  B  C  D  E  F  G  H
-0  1  2  3  4  5  6  7    0
-8  9  10 11 12 13 14 15   1 
-16 17 18 19 20 21 22 23   2
-24 25 26 27 28 29 30 31   3
-32 33 34                  4 */
